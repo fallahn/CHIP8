4c2aace2da46000b8774a349d22d7c2aa49c3ee5
fallahn <matty_styles@hotmail.com>
2016-03-02 19:48:49 +0000

    update test program with resolution switching

--- Chip8/include/CHIP8/TestRom.hpp
+++ Chip8/include/CHIP8/TestRom.hpp
@@ -35,10 +35,15 @@ source distribution.
 #include <vector>
 //TODO this should check as many opcodes as possible
 //TODO display a pixel for each test passed
-//TODO instructions should start on even number addresses
+
+//V0, V1 sprite coords
+//V2 timer counter
+//V3 loop counter
+//V4 hires state
+
 static const std::vector<sf::Uint8> testRom = //TODO convert to std::array
 {
-    0x12, 0x47,                   //0x200 skip charset / subs and start program
+    0x12, 0x48,                   //0x200 skip charset / subs and start program
     //charset
     0x70, 0x80, 0x80, 0x80, 0x70, //0x202 C
     0xF0, 0x80, 0xE0, 0x80, 0xF0, //0x207 E
@@ -51,62 +56,79 @@ static const std::vector<sf::Uint8> testRom = //TODO convert to std::array
     0xE0, 0x40, 0x40, 0x40, 0x40, //0x22A T
     0x40, 0x40, 0x40, 0x00, 0x40, //0x22F !
     0x60, 0x90, 0x60, 0x90, 0x60, //0x234 8
-    //draw then delay loop (subroutine)
-    0x62, 0x0F,                   //0x239 set V2 to 15
-    0xF2, 0x15,                   //0x23B set timer to V2
-    0xD0, 0x15,                   //0x23D draw V0, V1 for 5 bytes
-    0x42, 0x00,                   //0x23F skip next if V2 not 0
-    0x00, 0xEE,                   //0x241 quit subroutine
-    0xF2, 0x07,                   //0x243 copy timer to V2
-    0x12, 0x3F,                   //0x245 goto skip check
-    //program begin
-    0xA2, 0x02,                   //0x247 set I reg to C
-    0x60, 0x01,                   //0x249 set V0 to X coord
-    0x61, 0x01,                   //0x24B set V1 to Y coord
-    0x22, 0x39,                   //0x24D goto draw/delay sub
-    0xA2, 0x0C,                   //0x24F set I reg to H
-    0x60, 0x06,                   //0x251 set V0 to X coord
-    0x22, 0x39,                   //0x253 goto draw/delay sub
-    0xA2, 0x11,                   //0x255 set I reg to I
-    0x60, 0x0B,                   //0x257 set V0 to X coord
-    0x22, 0x39,                   //0x259 goto draw/delay sub
-    0xA2, 0x20,                   //0x25B set I reg to P
-    0x60, 0x0F,                   //0x25D set V0 to X coord
-    0x22, 0x39,                   //0x25F goto draw/delay sub
-    0xA2, 0x34,                   //0x261 set I reg to 8
-    0x60, 0x17,                   //0x263 set V0 to X coord
-    0x22, 0x39,                   //0x265 goto draw/delay sub
-    0xA2, 0x2A,                   //0x267 set I reg to T
-    0x60, 0x01,                   //0x269 set V0 to X coord
-    0x61, 0x07,                   //0x26B set V1 to Y coord
-    0x22, 0x39,                   //0x26D goto draw/delay sub
-    0xA2, 0x07,                   //0x26F set I reg to E
-    0x60, 0x05,                   //0x271 set V0 to X coord
-    0x22, 0x39,                   //0x273 goto draw/delay sub
-    0xA2, 0x25,                   //0x275 set I reg to S
-    0x60, 0x0A,                   //0x277 set V0 to X coord
-    0x22, 0x39,                   //0x279 goto draw/delay sub
-    0xA2, 0x2A,                   //0x27B set I reg to T
-    0x60, 0x0F,                   //0x27D set V0 to X coord
-    0x22, 0x39,                   //0x27F goto draw/delay sub
-    0xA2, 0x1B,                   //0x281 set I reg to O
-    0x60, 0x15,                   //0x283 set V0 to X coord
-    0x22, 0x39,                   //0x285 goto draw/delay sub
-    0xA2, 0x16,                   //0x287 set I reg to K
-    0x60, 0x1A,                   //0x289 set V0 to X coord
-    0x22, 0x39,                   //0x28B goto draw/delay sub
-    0xA2, 0x2F,                   //0x28D set I reg to !
-    0x60, 0x1F,                   //0x28F set V0 to X coord
-    0x22, 0x39,                   //0x291 goto draw/delay sub
+    0x00,                         //0x239 pad first instruction to even number address
     
-    0x82, 0x14,                   //0x293 set V2 to 20
-    0xF2, 0x18,                   //0x295 set sound timer to V2
-    0x62, 0x3C,                   //0x297 set V2 to 60
-    0xF2, 0x15,                   //0x299 set timer to V2
-    0x42, 0x00,                   //0x29B skip next if V2 not 0
-    0x12, 0x47,                   //0x29D goto start
-    0xF2, 0x07,                   //0x29F copy timer to V2
-    0x12, 0x9B                    //0x2A1 goto skip check
+    //draw then delay loop (subroutine)
+    0x62, 0x07,                   //0x23A set V2 to 7
+    0xF2, 0x15,                   //0x23C set timer to V2
+    0xD0, 0x15,                   //0x23E draw V0, V1 for 5 bytes
+    0x42, 0x00,                   //0x240 skip next if V2 not 0
+    0x00, 0xEE,                   //0x242 quit subroutine
+    0xF2, 0x07,                   //0x244 copy timer to V2
+    0x12, 0x40,                   //0x246 goto skip check
+    
+    //program begin
+    0xA2, 0x02,                   //0x248 set I reg to C
+    0x60, 0x01,                   //0x24A set V0 to X coord
+    0x61, 0x01,                   //0x24C set V1 to Y coord
+    0x22, 0x3A,                   //0x24E goto draw/delay sub
+    0xA2, 0x0C,                   //0x250 set I reg to H
+    0x60, 0x06,                   //0x252 set V0 to X coord
+    0x22, 0x3A,                   //0x254 goto draw/delay sub
+    0xA2, 0x11,                   //0x256 set I reg to I
+    0x60, 0x0B,                   //0x258 set V0 to X coord
+    0x22, 0x3A,                   //0x25A goto draw/delay sub
+    0xA2, 0x20,                   //0x25C set I reg to P
+    0x60, 0x0F,                   //0x25E set V0 to X coord
+    0x22, 0x3A,                   //0x260 goto draw/delay sub
+    0xA2, 0x34,                   //0x262 set I reg to 8
+    0x60, 0x17,                   //0x264 set V0 to X coord
+    0x22, 0x3A,                   //0x266 goto draw/delay sub
+    0xA2, 0x2A,                   //0x268 set I reg to T
+    0x60, 0x01,                   //0x26A set V0 to X coord
+    0x61, 0x07,                   //0x26C set V1 to Y coord
+    0x22, 0x3A,                   //0x26E goto draw/delay sub
+    0xA2, 0x07,                   //0x270 set I reg to E
+    0x60, 0x05,                   //0x272 set V0 to X coord
+    0x22, 0x3A,                   //0x274 goto draw/delay sub
+    0xA2, 0x25,                   //0x276 set I reg to S
+    0x60, 0x0A,                   //0x278 set V0 to X coord
+    0x22, 0x3A,                   //0x27A goto draw/delay sub
+    0xA2, 0x2A,                   //0x27C set I reg to T
+    0x60, 0x0F,                   //0x27E set V0 to X coord
+    0x22, 0x3A,                   //0x280 goto draw/delay sub
+    0xA2, 0x1B,                   //0x282 set I reg to O
+    0x60, 0x15,                   //0x284 set V0 to X coord
+    0x22, 0x3A,                   //0x286 goto draw/delay sub
+    0xA2, 0x16,                   //0x288 set I reg to K
+    0x60, 0x1A,                   //0x28A set V0 to X coord
+    0x22, 0x3A,                   //0x28C goto draw/delay sub
+    0xA2, 0x2F,                   //0x28E set I reg to !
+    0x60, 0x1F,                   //0x290 set V0 to X coord
+    0x22, 0x3A,                   //0x292 goto draw/delay sub
+    
+    //count number of times test loops and toggle res mode
+    0x73, 0x01,                   //0x294 increment demo loop counter V3
+    0x33, 0x02,                   //0x296 skip instruction if V3 == 2
+    0x12, 0xAA,                   //0x298 jump to delay loop at end
+    0x63, 0x00,                   //0x29A reset V3, loop counter, to zero
+    0x34, 0x00,                   //0x29C if set to lowres skip to setting hires
+    0x12, 0xA6,                   //0x29E jump to setting low res
+    0x00, 0xFF,                   //0x2A0 switch to hires mode
+    0x64, 0x01,                   //0x2A2 set hires flag
+    0x12, 0xAA,                   //0x2A4 jump to delay loop at end
+    0x00, 0xFE,                   //0x2A6 switch to lowres
+    0x64, 0x00,                   //0x2A8 set lowres flag
+
+    //delay loop before restarting
+    0x82, 0x14,                   //0x2AA set V2 to 20
+    0xF2, 0x18,                   //0x2AC set sound timer to V2
+    0x62, 0x3C,                   //0x2AE set V2 to 60
+    0xF2, 0x15,                   //0x2B0 set timer to V2
+    0x42, 0x00,                   //0x2B2 skip next if V2 not 0
+    0x12, 0x48,                   //0x2B4 goto start
+    0xF2, 0x07,                   //0x2B6 copy timer to V2
+    0x12, 0xB2                    //0x2B8 goto skip check
 };
 
 //CHIP8 TEST OK!
